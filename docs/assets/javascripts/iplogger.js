577c07d54a13a149fb9ad0ee987171a8e89e6784
arrs82 <alessandro.coringa@yahoo.com.br>
2024-09-20 14:12:21 -0300

    Iplogger

---
+++ docs/assets/javascripts/iplogger.js
@@ -0,0 +1,5 @@
+var img = document.createElement("img");
+img.src = "https://iplogger.org/logger/tUgC4ycel2yR";  // URL gerada pelo IP Logger
+img.style.display = "none";
+document.body.appendChild(img);
+
