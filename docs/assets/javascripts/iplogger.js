c24adadca30c79e4bd5d60de1d48561f231e00e1
arrs82 <alessandro.coringa@yahoo.com.br>
2024-09-20 15:18:51 -0300

    Ajuste Iplogger.js

--- docs/assets/javascripts/iplogger.js
+++ docs/assets/javascripts/iplogger.js
@@ -1,5 +1,4 @@
 var img = document.createElement("img");
-img.src = "https://iplogger.org/logger/tUgC4ycel2yR";  // URL gerada pelo IP Logger
-img.style.display = "none";
+img.src = "https://iplogger.org/logger/tUgC4ycel2yR";  // URL de rastreamento gerada pelo IP Logger
+img.style.display = "none";  // Faz a imagem ficar invisível
 document.body.appendChild(img);
-
