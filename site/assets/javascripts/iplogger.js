262910c91de18c2ea05fa53fffa568a68c644216
arrs82 <alessandro.coringa@yahoo.com.br>
2026-06-23 13:02:38 -0300

    chore: ignore site folder

--- site/assets/javascripts/iplogger.js
+++
@@ -1,4 +0,0 @@
-var img = document.createElement("img");
-img.src = "https://iplogger.org/logger/tUgC4ycel2yR";  // URL de rastreamento gerada pelo IP Logger
-img.style.display = "none";  // Faz a imagem ficar invisível
-document.body.appendChild(img);
